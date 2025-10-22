15325e6069a9cc97db91528f17db2745b274bcff
Mohsin Raza <mr.mohsinraza32@gmail.com>
2025-10-22 14:24:03 +0500

    changes

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+ $('.hero-section').slick({
+    autoplay: false,
+    autoplaySpeed: 4000,
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    fade: true,
+    cssEase: 'linear',
+    pauseOnHover: false,
+  });
+});
